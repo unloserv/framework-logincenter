fc3ecdf41c3ddc954f793cd791ea9db0e96f3f08
caoshuai <unloserv@126.com>
2019-02-25 09:04:01 +0800

    update bing story request

---
+++ src/main/java/com/canghuang/logincenter/core/BingStory/BingStory.java
@@ -0,0 +1,38 @@
+package com.canghuang.logincenter.core.BingStory;
+
+import lombok.Setter;
+
+/**
+ * @author cs
+ * @date 2019/2/25
+ * @description
+ */
+@Setter
+public class BingStory {
+
+  private String title;
+
+  private String attribute;
+
+  private String image;
+
+  private String story;
+
+  private String search;
+
+  private String provider;
+
+  private String Continent;
+
+  private String Country;
+
+  private String City;
+
+  private String Longitude;
+
+  private String Latitude;
+
+  private String date;
+
+
+}
